120c87dbad332caa29553479829bdca2ad25200e
Duncan Stead <duncan.robert.stead@gmail.com>
2021-05-19 14:41:18 +0100

    In the WebRTC driver, just connect straight to location.hostname instead of whatever IP the client wanted :)

--- net_drivers/webrtc/js/api.js
+++ net_drivers/webrtc/js/api.js
@@ -99,7 +99,9 @@ mergeInto(LibraryManager.library, {
 
     __js_game_client_start: function(hostPtr, port) {
         return Asyncify.handleSleep(function (wakeUp) {
-            this.gameClient.connect(UTF8ToString(hostPtr), port).then(() => {
+			// Ignore the ip and just connect to the web host address
+			console.log("Connecting to " + location.hostname);
+            this.gameClient.connect(location.hostname, port).then(() => {
                 wakeUp(0)
             }).catch(_ => {
                 wakeUp(-1)
