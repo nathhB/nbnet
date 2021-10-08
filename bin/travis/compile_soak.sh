c2efb6e78f110ea945481fdc7660267c5b787a9a
BIAGINI Nathan <nath.biagini@gmail.com>
2021-10-08 23:09:06 +0200

    link with libm on unix

--- bin/travis/compile_soak.sh
+++ bin/travis/compile_soak.sh
@@ -18,7 +18,7 @@ then
         mingw32-make
     else
         C:/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/BuildTools/MSBuild/Current/Bin/MSBuild.exe -p:Configuration=Debug soak.sln
-        ls -l
+        ls -l Debug
     fi
 else
     make
