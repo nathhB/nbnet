e822abe3a436149e6b1b6cd2d13a7ae0d190470f
BIAGINI Nathan <nath.biagini@gmail.com>
2021-10-09 00:14:47 +0200

    travis, fix mingw build + add logs

--- bin/travis/run_soak.sh
+++ bin/travis/run_soak.sh
@@ -2,11 +2,14 @@
 
 cd soak/build
 
-if [ "$TRAVIS_OS_NAME" = "windows" ] && [ "$CMAKE_GENERATOR" != "MinGW Makefiles" ]
+if [ "$TRAVIS_OS_NAME" = "windows" ]
 then
-    # MSVC
+    if [ "$CMAKE_GENERATOR" != "MinGW Makefiles" ]
+    then
+        # MSVC
 
-    cd Debug # go to VS Debug folder that contains client.exe and server.exe
+        cd Debug # go to VS Debug folder that contains client.exe and server.exe
+    fi
 fi
 
 echo "Starting soak server..."
@@ -19,5 +22,9 @@ echo "Server started (PID: $SERV_PID)"
 echo "Running soak test..."
 
 ./client --message_count=100 &> soak_cli_out
+
+echo "Printing end of client logs..."
+
+cat soak_cli_out | grep -A10 -B10 "Received all soak message echoes"
 
 kill $SERV_PID
