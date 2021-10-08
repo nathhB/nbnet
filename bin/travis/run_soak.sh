b8491ed234a786101ec53add35d0d475a9abd0df
BIAGINI Nathan <nath.biagini@gmail.com>
2021-10-08 11:29:44 +0200

    travis, redirect logs

--- bin/travis/run_soak.sh
+++ bin/travis/run_soak.sh
@@ -2,11 +2,12 @@
 
 cd soak/build
 
-echo "Booting server..."
+echo "Booting soak server..."
 
 ./server --packet_loss=0.4 --packet_duplication=0.5 --ping=0.4 --jitter=0.2 &
 sleep 2
 
-echo "Booting client..."
+echo "OK."
+echo "Running soak client..."
 
-./client --message_count=100 --packet_loss=0.6 --packet_duplication=0.5 --ping=0.2 --jitter=0.1
+./client --message_count=100 --packet_loss=0.6 --packet_duplication=0.5 --ping=0.2 --jitter=0.1 &> soak_cli_out
