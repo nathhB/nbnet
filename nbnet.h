6534712c38d43a48d0fdccff8f42caa6d1b737a4
BIAGINI Nathan <nath.biagini@gmail.com>
2021-10-08 22:22:45 +0200

    remove designated initializer list for MSVC compatibility

--- nbnet.h
+++ nbnet.h
@@ -2763,7 +2763,7 @@ static void Connection_InitOutgoingPacket(
 static NBN_PacketEntry *Connection_InsertOutgoingPacketEntry(NBN_Connection *connection, uint16_t seq_number)
 {
     uint16_t index = seq_number % NBN_MAX_PACKET_ENTRIES;
-    NBN_PacketEntry entry = { .acked = false, .messages_count = 0, .send_time = 0 };
+    NBN_PacketEntry entry = { false, 0, 0 };
 
     connection->packet_send_seq_buffer[index] = seq_number;
     connection->packet_send_buffer[index] = entry;
