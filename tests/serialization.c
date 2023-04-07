64dca128a61ab40e6e4654adb644018b2ad5e4de
BIAGINI Nathan <nath.biagini@gmail.com>
2023-04-07 12:28:26 +0200

    fix serialization unit tests

--- tests/serialization.c
+++ tests/serialization.c
@@ -9,6 +9,7 @@
 #define NBN_LogTrace printf
 #define NBN_LogDebug printf
 #define NBN_LogError printf
+#define NBN_LogWarning printf
 
 #define NBN_Allocator malloc
 #define NBN_Deallocator free
@@ -24,9 +25,27 @@ typedef struct
 
 int BogusMessage_Serialize(BogusMessage *msg, NBN_Stream *stream)
 {
-    SERIALIZE_FLOAT(msg->v1, -100, 100, 1);
-    SERIALIZE_FLOAT(msg->v2, -100, 100, 2);
-    SERIALIZE_FLOAT(msg->v3, -100, 100, 3);
+    NBN_SerializeFloat(stream, msg->v1, -100, 100, 1);
+    NBN_SerializeFloat(stream, msg->v2, -100, 100, 2);
+    NBN_SerializeFloat(stream, msg->v3, -100, 100, 3);
+
+    return 0;
+}
+
+typedef struct
+{
+    uint64_t v1;
+    uint64_t v2;
+    uint64_t v3;
+    uint64_t v4;
+} BogusMessage2;
+
+int BogusMessage2_Serialize(BogusMessage2 *msg, NBN_Stream *stream)
+{
+    NBN_SerializeUInt64(stream, msg->v1);
+    NBN_SerializeUInt64(stream, msg->v2);
+    NBN_SerializeUInt64(stream, msg->v3);
+    NBN_SerializeUInt64(stream, msg->v4);
 
     return 0;
 }
@@ -54,12 +73,40 @@ void Test_SerializeFloat(CuTest *tc)
     CuAssertIntEquals(tc, -89123, r_msg.v3 * 1000);
 }
 
+void Test_SerializeUInt64(CuTest *tc)
+{
+    BogusMessage2 msg = {
+        .v1 = 0xFFFFFFFFFFFFFFFF,
+        .v2 = 9223372036854775807,
+        .v3 = 4611686018427387903,
+        .v4 = 42000
+    };
+    NBN_WriteStream w_stream;
+    uint8_t buffer[32];
+
+    NBN_WriteStream_Init(&w_stream, buffer, sizeof(buffer));
+    CuAssertIntEquals(tc, 0, BogusMessage2_Serialize(&msg, (NBN_Stream *)&w_stream));
+    NBN_WriteStream_Flush(&w_stream);
+
+    BogusMessage2 r_msg;
+    NBN_ReadStream r_stream;
+
+    NBN_ReadStream_Init(&r_stream, buffer, sizeof(buffer));
+
+    CuAssertIntEquals(tc, 0, BogusMessage2_Serialize(&r_msg, (NBN_Stream *)&r_stream));
+    CuAssertTrue(tc, r_msg.v1 == 0xFFFFFFFFFFFFFFFF);
+    CuAssertTrue(tc, r_msg.v2 == 9223372036854775807);
+    CuAssertTrue(tc, r_msg.v3 == 4611686018427387903);
+    CuAssertTrue(tc, r_msg.v4 == 42000);
+}
+
 int main(int argc, char *argv[])
 {
     CuString *output = CuStringNew();
     CuSuite* suite = CuSuiteNew();
 
     SUITE_ADD_TEST(suite, Test_SerializeFloat);
+    SUITE_ADD_TEST(suite, Test_SerializeUInt64);
 
     CuSuiteRun(suite);
     CuSuiteSummary(suite, output);
