0e1974d28788dfcc89f75b08fe9669aefd8135b2
BIAGINI Nathan <nath.biagini@gmail.com>
2024-02-07 22:28:54 +0100

    update copyright year

--- soak/soak.h
+++ soak/soak.h
@@ -1,6 +1,6 @@
 /*
 
-   Copyright (C) 2023 BIAGINI Nathan
+   Copyright (C) 2024 BIAGINI Nathan
 
    This software is provided 'as-is', without any express or implied
    warranty.  In no event will the authors be held liable for any damages
