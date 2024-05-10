3273b48f1d2ab898ec72f076cb7d3c09d9eeb7d6
BIAGINI Nathan <nath.biagini@gmail.com>
2024-05-10 08:53:02 +0200

    fix examples

--- examples/rpc/client.c
+++ examples/rpc/client.c
@@ -69,13 +69,8 @@ int main(int argc, char *argv[])
 #endif // __EMSCRIPTEN__
 
     // Initialize the client with a protocol name (must be the same than the one used by the server), the server ip address and port
-#ifdef NBN_ENCRYPTION
-    bool enable_encryption = true;
-#else
-    bool enable_encryption = false;
-#endif
 
-    if (NBN_GameClient_StartEx(RPC_PROTOCOL_NAME, "127.0.0.1", RPC_EXAMPLE_PORT, enable_encryption, NULL, 0) < 0)
+    if (NBN_GameClient_StartEx(RPC_PROTOCOL_NAME, "127.0.0.1", RPC_EXAMPLE_PORT, NULL, 0) < 0)
     {
         Log(LOG_ERROR, "Failed to start client");
 
