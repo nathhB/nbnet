3c77c2501b17a70d1c36d17c3008ebcaec2fcb80
BIAGINI Nathan <nath.biagini@gmail.com>
2024-09-12 11:05:21 +0200

    emscripten 3.1.64

--- bin/github-actions/install_emsdk.sh
+++ bin/github-actions/install_emsdk.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
-./emsdk install latest
-./emsdk activate latest
+./emsdk install 3.1.64
+./emsdk activate 3.1.64
 source ./emsdk_env.sh
 emcc -v # make sure emcc is available
