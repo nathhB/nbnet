989e04dcf0caf9b28c9d5731c5bc5a3a8d2c2361
BIAGINI Nathan <nath.biagini@gmail.com>
2026-01-28 21:05:44 +0100

    fix run soak pipeline script

--- bin/github-actions/run_soak.sh
+++ bin/github-actions/run_soak.sh
@@ -8,21 +8,22 @@ CHANNEL_COUNT=3
 MESSAGE_COUNT=500
 NODE_CMD="$EMSDK_NODE"
 
-run_client () {
-    node_client=$1
-    echo "Running soak client (run in mode: $node_client)..."
+run_client() {
+    client_mode=$1
+    echo "Running soak client (run in mode: $client_mode)..."
 
-    if [ $node_client -eq 1 ]
-    then
+    if [ "$client_mode" = "WEBRTC_EMSCRIPTEN" ]; then
         # WASM WebRTC client
-        $NODE_CMD build_web/client.js --message_count=$MESSAGE_COUNT --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &> soak_cli_out
-    elif [ $node_client -eq 2 ]
-    then
+        $NODE_CMD build_web/client.js --message_count=$MESSAGE_COUNT --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &>soak_cli_out
+    elif [ "$client_mode" = "WEBRTC_NATIVE" ]; then
         # native WebRTC client
-        ./build/client --webrtc --message_count=$MESSAGE_COUNT --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &> soak_cli_out
-    else
+        ./build/client --webrtc --message_count=$MESSAGE_COUNT --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &>soak_cli_out
+    elif [ "$client_mode" = "UDP" ]; then
         # UDP client
-        ./build/client --message_count=$MESSAGE_COUNT --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &> soak_cli_out
+        ./build/client --message_count=$MESSAGE_COUNT --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &>soak_cli_out
+    else
+        echo "Unknown client mode"
+        return 1
     fi
 
     RESULT=$?
@@ -30,15 +31,11 @@ run_client () {
     # when running the soak test in the latest version of emscripten with node 16
     # the client aborts at the end when calling emscripten_force_exit
     # I could not figure out why, hence the condition
-    [[ $node_client -eq 1 ]] && EXPECTED_RESULT=7 || EXPECTED_RESULT=0
-    
-    if [ $RESULT -eq $EXPECTED_RESULT ]
-    then
+    [[ "$client_mode" = "WEBRTC_EMSCRIPTEN" ]] && EXPECTED_RESULT=7 || EXPECTED_RESULT=0
+
+    if [ $RESULT -eq $EXPECTED_RESULT ]; then
         echo "Soak test completed with success!"
-        echo "Printing the end of client logs..."
-    
-        tail -n 150 soak_cli_out
-    
+
         return 0
     else
         echo "Soak test failed! (code: $RESULT)"
@@ -51,8 +48,8 @@ run_client () {
     fi
 }
 
-exit_soak () {
-    kill -SIGINT $SERV_PID 2> /dev/null
+exit_soak() {
+    kill -SIGINT $SERV_PID 2>/dev/null
 
     exit $1
 }
@@ -60,15 +57,13 @@ exit_soak () {
 cd soak
 echo "Starting soak server..."
 
-if [ -n "$WEBRTC" ]
-then
-     $NODE_CMD build_web/server.js --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &> soak_serv_out &
+if [ -n "$WEBRTC" ]; then
+    $NODE_CMD build_web/server.js --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &>soak_serv_out &
 else
-    ./build/server --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &> soak_serv_out &
+    ./build/server --channel_count=$CHANNEL_COUNT --packet_loss=$PACKET_LOSS --packet_duplication=$PACKET_DUPLICATION --ping=$PING --jitter=$JITTER &>soak_serv_out &
 fi
 
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
     SERV_PID=$!
 
     echo "Server started (PID: $SERV_PID)"
@@ -80,20 +75,20 @@ fi
 
 sleep 3
 
-if [ -n "$WEBRTC" ]
-then
-    run_client 1
+if [ -n "$WEBRTC" ]; then
+    run_client "WEBRTC_EMSCRIPTEN"
 
     exit_soak $?
-else
-    if [ -n "$WEBRTC_NATIVE" ]
-    then
-        # run a UDP client, a webrtc WASM client (emscripten) and a native webrtc client (all connecting to the same server)
+elif [ -n "$WEBRTC_NATIVE" ]; then
+    # run a UDP client, a webrtc WASM client (emscripten) and a native webrtc client (all connecting to the same server)
 
-        if run_client 0 && run_client 1 && run_client 2; then
-            exit_soak 0
-        else
-            exit_soak 1
-        fi
+    if run_client "UDP" && run_client "WEBRTC_EMSCRIPTEN" && run_client "WEBRTC_NATIVE"; then
+        exit_soak 0
+    else
+        exit_soak 1
     fi
+else
+    run_client "UDP"
+
+    exit_soak $?
 fi
