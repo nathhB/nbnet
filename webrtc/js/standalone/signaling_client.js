72e9da0c252e5a9b5c47453267b1066c0a724db2
BIAGINI Nathan <nath.biagini@gmail.com>
2026-02-14 09:21:21 +0100

    readd webrtc js code removed by mistake

---
+++ webrtc/js/standalone/signaling_client.js
@@ -0,0 +1,86 @@
+const loggerFactory = require('../logger.js')
+
+function SignalingClient(protocol_id, options) {
+    this.protocol = protocol_id.toString()
+    this.logger = loggerFactory.createLogger('StandaloneSignalingClient')
+    this.connected = false
+    this.options = options
+}
+
+SignalingClient.prototype.connect = function(host, port) {
+    return new Promise((resolve, reject) => {
+        const uri = this.options['https'] ? `wss://${host}:${port}` : `ws://${host}:${port}`
+
+        this.logger.info(this.options['https'])
+        this.logger.info(`Connecting to ${uri}...`)
+
+        const WebSocket = require('websocket').w3cwebsocket
+
+        this.ws = new WebSocket(uri)
+
+        this.ws.onclose = (ev) => {
+            if (this.connected) {
+                this.logger.error('Connection closed')
+
+                this.connected = false
+
+                this.onClosed()
+            } else {
+                this.logger.error('Connection failed')
+
+                reject()
+            }
+        }
+
+        this.ws.onopen = () => {
+            this.logger.info('Connected')
+
+            this.connected = true
+            
+            clearTimeout(timeoutId)
+            resolve()
+        }
+
+        this.ws.onmessage = (ev) => {
+            this.logger.info('Received signaling data: %s', ev.data)
+
+            this.onDataReceived(JSON.parse(ev.data))
+        }
+
+        const timeoutId = setTimeout(() => {
+            this.logger.error('Connection timeout')
+
+            reject()
+        }, 3000)
+    })
+}
+
+SignalingClient.prototype.send = function(data) {
+    this.logger.info('Send signaling data: %s', data)
+
+    this.ws.send(JSON.stringify(data))
+}
+
+SignalingClient.prototype.close = function() {
+    this.logger.info('Closing...')
+
+    const WebSocket = require('websocket').w3cwebsocket
+
+    return new Promise((resolve, reject) => {
+        if (this.ws.readyState != WebSocket.OPEN) {
+            this.logger.warn('Not opened')
+
+            reject()
+        } else {
+            this.ws.onclose = (_) => {
+                this.logger.info('Closed')
+
+                resolve()
+            }
+
+            this.ws.close()
+        }
+    })
+}
+
+module.exports = SignalingClient
---
+++ webrtc/js/standalone/signaling_server.js
@@ -0,0 +1,79 @@
+const Connection = require('./connection.js')
+const loggerFactory = require('../logger.js')
+
+function SignalingServer(protocol_id, options) {
+    this.protocol = protocol_id.toString()
+    this.logger = loggerFactory.createLogger('StandaloneSignalingServer')
+    this.options = options
+}
+
+SignalingServer.prototype.start = function(port) {
+    return new Promise((resolve, reject) => {
+        this.logger.info('Starting (protocol: %s)...', this.protocol)
+
+        var server
+
+        if (this.options['https']) {
+            const fs = require('fs')
+
+            server = createHttpsServer(this, fs.readFileSync(this.options['key']), fs.readFileSync(this.options['cert']))
+        } else {
+            server = createHttpServer(this)
+        }
+
+        const WebSocketServer = require('websocket').server
+
+        this.wsServer = new WebSocketServer({
+            httpServer: server,
+            autoAcceptConnections: false
+        })
+
+        this.wsServer.on('request', (request) => {
+            this.logger.info('New connection')
+
+            try {
+                this.onConnection(new Connection(request.accept(null, request.origin)))
+            } catch (err) {
+                this.logger.error('Connection rejected: %s', err)
+            }
+        })
+
+        server.listen(port, () => {
+            this.logger.info('Started, listening on port %d...', port);
+
+            resolve()
+        })
+    })
+}
+
+SignalingServer.prototype.stop = function() {
+    if (this.wsServer) {
+        this.wsServer.shutDown()
+    } else {
+        this.logger.error("Not started")
+    }
+}
+
+SignalingServer.prototype.isSecure = function() {
+    return this.options['https']
+}
+
+function createHttpServer(signalingServer) {
+    return require('http').createServer((request, response) => {
+        signalingServer.logger.info('Received request for ' + request.url)
+
+        response.writeHead(404)
+        response.end()
+    })
+}
+
+function createHttpsServer(signalingServer, key, cert) {
+    return require('https').createServer({ key: key, cert: cert }, (request, response) => {
+        signalingServer.logger.info('Received request for ' + request.url)
+
+        response.writeHead(404)
+        response.end()
+    })
+}
+
+module.exports = SignalingServer
